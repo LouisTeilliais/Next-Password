510b768a6db6d37e810d350b1be6ee510e984a6c
Louis <louis.teilliais@gmail.com>
2024-02-09 15:35:51 +0100

    init nav

---
+++ NextPassword/MVVM/Views/Register.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NextPassword.MVVM.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour Page1.xaml
+    /// </summary>
+    public partial class Register : Page
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        /*private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            // Open the URL in the default web browser
+            NavigationService.Navigate(new Uri("Login.xaml"));
+            
+        }*/
+    }
+}
